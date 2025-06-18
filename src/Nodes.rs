fe08f057078d4f98941c2fe999b14af51efed6a9
Yahli Mariansky <yahlimem@gmail.com>
2025-06-18 14:39:50 +0300

    fixed but still not good

--- src/Nodes.rs
+++ src/Nodes.rs
@@ -27,34 +27,41 @@ impl Node {
         self.angle += self.anglular_velocity * dt;
     }
     /// distance is the vector from out position to the forces position
-    pub fn torque(&self, connected_node: &Option<Node>) -> f32{
+    pub fn torque(&self, last_node: &Option<Node>, next_node: &Option<Node>) -> f32{
         let liqued_speed = 10.0;
         let liqued_density = 1.0;
-        if let Some(connected_node) = connected_node { // TODO: fix angle
-            let force1: Vec2 = connected_node.liqued_force(connected_node.angle - self.angle, liqued_speed, liqued_density); // the direction of where the pipe is looking
-            // let force2: Vec2 = -self.liqued_force(liqued_speed, liqued_density); // the direction of where the pipe is looking
-            // let recistance = force1 ity;
-            return Self::corss_product(force1, (self.end_pos - self.mid_pos))/* + Self::corss_product(force2, (self.end_pos - self.mid_pos))*/;
+        let force1;
+        let force2;
+        if let Some(last_node) = last_node {
+            force1 = self.liqued_force(-(last_node.angle - self.angle), liqued_speed, liqued_density, &last_node); // the direction of where the pipe is looking
+        } else {
+            force1 = Vec2::ZERO;
         }
+        if let Some(next_node) = next_node {
+            force2 = next_node.liqued_force(-(self.angle - next_node.angle), liqued_speed, liqued_density, &self); // the direction of where the pipe is looking
+        } else {
+            force2 = Vec2::ZERO;
+        }
+        return Self::corss_product(force1, (self.pos - self.mid_pos)) + Self::corss_product(force2, (self.end_pos - self.mid_pos));
 
         // let force2: Vec2 = self.liqued_force(liqued_speed, liqued_density); // the direction of where the pipe is looking
 
-        return 0.0;
     }
 
-    pub fn liqued_force(&self, angle: f32, liqued_speed: f32, liqued_density: f32) -> Vec2{
-        liqued_density*liqued_speed*liqued_speed * (0.5*angle).sin() * 0.01 * 2.0 * vec2(self.angle.cos(), self.angle.sin()) /* 0.01 meanis that the pipe segment is one centimeter */
+    pub fn liqued_force(&self, angle: f32, liqued_speed: f32, liqued_density: f32, other_node: &Node) -> Vec2{
+        // if both angles are the same return zero
+        if ((vec2(self.angle.cos(), self.angle.sin()) - vec2(other_node.angle.cos(), other_node.angle.sin())).length_squared() == 0.0) {return Vec2::ZERO}
+        return liqued_density*liqued_speed*liqued_speed * (0.5*angle).sin() * 0.01 * (vec2(other_node.angle.cos(), other_node.angle.sin()) - vec2(self.angle.cos(), self.angle.sin())).normalize() /* 0.01 meanis that the pipe segment is one centimeter */
     }
 
     pub fn dumpin(&self) -> Vec2{
         let angle = self.angle + 0.5*PI;
         Vec2::ZERO
-        // vec2(angle.cos(), angle.sin()) * self.anglular_velocity * 0.1
     }
 
-    pub fn calculate_position(&mut self, connected_node: &Option<Node>){
-        if let Some(connected_node) = connected_node {
-            self.pos = connected_node.end_pos;
+    pub fn calculate_position(&mut self, last_node: &Option<Node>){
+        if let Some(last_node) = last_node {
+            self.pos = last_node.end_pos;
         } else {
             self.pos = vec2(0.0, -200.0);
 
@@ -63,26 +70,33 @@ impl Node {
         self.mid_pos = vec2(self.angle.cos(), self.angle.sin()) * self.radius + self.pos;
     }
 
-    pub fn step(&mut self, dt: f32, connected_node: Option<Node> /* clone */){
-        let torque = self.torque(&connected_node);
+    pub fn step(&mut self, dt: f32, last_node: &Option<Node>, next_node: &Option<Node>){
+        let torque = self.torque(&last_node, &next_node);
         let inertia = self.mass*(1.0/12.0)*self.radius*self.radius;
         self.anglular_acceleration = torque/inertia;
 
         self.solver(dt);
         
-        self.calculate_position(&connected_node);
+        self.calculate_position(&last_node);
     }
 
-    pub fn draw(&self, draw: &Draw, connected_node: &Option<Node>, color: nannou::prelude::Hsl){
-        if let Some(connected_node) = connected_node {
-            let force1: Vec2 = connected_node.liqued_force(connected_node.angle - self.angle, 10.0, 0.1); // the direction of where the pipe is looking
+    pub fn draw(&self, draw: &Draw, last_node: &Option<Node>, next_node: &Option<Node>, color: nannou::prelude::Hsl){
+        if let Some(last_node) = last_node {
+            let force1: Vec2 = self.liqued_force(last_node.angle - self.angle, 10.0, 0.1, &last_node); // the direction of where the pipe is looking
+            // println!("{}, {}", last_node.end_pos, force1);
+            draw.arrow().start(last_node.end_pos).end(last_node.end_pos + (force1)*1000.0).stroke_weight(2.0).color(RED);
             // let force2: Vec2 = -self.liqued_force(10.0, 1.0); // the direction of where the pipe is looking
-            draw.arrow().start(connected_node.end_pos).end(connected_node.end_pos + (force1)*1000.0).stroke_weight(2.0).color(color);
             // draw.arrow().start(self.pos).end(self.pos + (-self.dumpin())*1000.0).stroke_weight(2.0).color(BLUE);
             // draw.arrow().start(self.pos).end(self.pos + (-self.dumpin() + force1)*1000.0).stroke_weight(2.0).color(PINK);
             // draw.arrow().start(self.pos).end(self.pos + vec2((connected_node.angle - self.angle).cos(), (connected_node.angle - self.angle).sin())*100.0).stroke_weight(2.0).color(PURPLE);
             // println!("angle: {}", (connected_node.angle - self.angle).rad_to_deg());
         }
+        if let Some(next_node) = next_node {
+            let force2: Vec2 = self.liqued_force(self.angle - next_node.angle, 10.0, 0.1, &next_node); // the direction of where the pipe is looking
+            draw.arrow().start(next_node.pos).end(next_node.pos + (force2)*1000.0).stroke_weight(2.0).color(RED);
+        }
+        // draw.ellipse().radius(10.0).xy(self.pos).color(BLUE);
+        // draw.ellipse().radius(10.0).xy(self.end_pos).color(BLUE);
         // draw.line().start(self.pos).end(self.end_pos).stroke_weight(10.0);
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, fmt::{format, Write}, rc::Rc};
 
-use nannou::{math::ConvertAngle, prelude::*};
+use nannou::{image::gif::GifEncoder, math::ConvertAngle, prelude::*};
 use nannou_egui::{self, egui, Egui};
 
 mod Nodes;
@@ -11,7 +11,8 @@ struct Model {
     chain_length: usize,
     link_length: f32,
     speed: u32,
-    chain: Vec<Node>
+    chain: Vec<Node>,
+    debug: bool,
 }
 
 fn main() {
@@ -19,15 +20,18 @@ fn main() {
     
 }
 
-fn generate_chain(length: usize, link_length: f32, mass: f32) -> Vec<Node>{
+fn generate_chain(length: usize, link_rad: f32, mass: f32) -> Vec<Node>{
     let mut nodes = Vec::with_capacity(length);
-    nodes.push(Node::new(PI/2.0, link_length, mass));
+    nodes.push(Node::new(PI/2.0, link_rad, mass));
+    nodes[0].step(0.00001, &None, &None);
     for i in 1..length {
-        nodes[i-1].step(0.00001, None);
+        nodes.push(Node::new(random_f32().abs() * 0.01*PI + PI/2.0, link_rad, mass));
 
-        nodes.push(Node::new(random_f32().abs() * PI + PI/2.0, link_length, mass));
+
+        let last_node = Some(nodes[i-1].clone());
+        nodes[i].step(0.00001, &last_node, &None);
+
     }
-    // println!("{}", nodes.len());
     return nodes;
 }
 
@@ -44,6 +48,7 @@ fn model(app: &App) -> Model {
         chain: generate_chain(chain_length, link_length, 1.0),
         speed: 1,
         link_length,
+        debug: true,
     }
 }
 
@@ -51,19 +56,24 @@ fn model(app: &App) -> Model {
 
 fn update(app: &App, model: &mut Model, update: Update) {
     // println!("{}", (app.mouse.position().y).atan2(app.mouse.position().x).rad_to_deg());
-    render_egui(&mut model.egui, &mut model.chain_length, &mut model.link_length, &mut model.speed, &mut model.chain);
-    let dt = 0.001;
+    render_egui(&mut model.egui, &mut model.chain_length, &mut model.link_length, &mut model.speed, &mut model.debug, &mut model.chain);
+    let dt = 0.0001;
     // println!("{:?}", &model.chain[9]);
     for _ in 0..model.speed*10{
         let clone_chain = model.chain.clone();
-        for node in 1..model.chain_length{
-            let current_link = Some(clone_chain[node-1].clone());
-            model.chain[node].step(dt, current_link);
+        
+        for node in 1..model.chain_length - 1{
+            let last_node = Some(clone_chain[node-1].clone());
+            let next_node = Some(clone_chain[node+1].clone());
+            model.chain[node].step(dt, &last_node, &next_node);
         }
+
+        let last_node = Some(clone_chain[model.chain_length - 2].clone());
+        model.chain.last_mut().unwrap().step(dt, &last_node, &None);
 
     }
 }
-fn render_egui(egui: &mut Egui, chain_length: &mut usize, link_length: &mut f32, speed: &mut u32, chain: &mut Vec<Node>){
+fn render_egui(egui: &mut Egui, chain_length: &mut usize, link_length: &mut f32, speed: &mut u32, debug: &mut bool, chain: &mut Vec<Node>){
     let egui = egui;
     // egui.set_elapsed_time(update.since_start);
 
@@ -77,12 +87,14 @@ fn render_egui(egui: &mut Egui, chain_length: &mut usize, link_length: &mut f32,
         ui.label("speed"); // template
         let speed = ui.add(egui::Slider::new(speed, 1..=100));
         let reset = ui.add(egui::Button::new("Reset"));
+        let debug_toggle = ui.add(egui::Checkbox::new(debug, "Debug"));
         // let slider = ui.add(egui::Slider::new(chain_length, 10..=500));
 
         if chain_length_slider.changed() || link_length_slider.changed() || reset.clicked() {
             *chain = generate_chain(*chain_length, *link_length, 1.0);
         }
     });
+    
 }
 
 fn raw_window_event(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent){
@@ -93,20 +105,50 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(WHITE);
 
+    if model.debug {
+        draw.line().start(model.chain[0].pos).end(model.chain[0].end_pos).color(BLACK).stroke_weight(3.0);
+        
+        draw.line().start(model.chain[model.chain_length - 1].pos).end(model.chain[model.chain_length - 1].end_pos).color(BLACK).stroke_weight(3.0);
+        
+        let last_node = Some(model.chain[model.chain_length - 2].clone());
+        model.chain.last().unwrap().draw(&draw, &last_node, &None, Hsl::new(0.0,0.0,0.0));
+        
+        
+        // let next_node = Some(model.chain[1].clone());
+        // model.chain.first().unwrap().draw(&draw, &None, &next_node, Hsl::new(0.0,0.0,0.0));
+        
+        for node in 1..model.chain_length - 1{
+            let color = Hsl::new((node as f32)/(model.chain_length as f32)*360.0,1.0,0.5);
+            draw.line().start(model.chain[node].pos).end(model.chain[node].end_pos).color(color).stroke_weight(3.0);
+    
+    
+            let last_node = Some(model.chain[node-1].clone());
+            let next_node = Some(model.chain[node+1].clone());
+            model.chain[node].draw(&draw, &last_node, &next_node, color);
+        }
 
-    draw.line().start(model.chain[0].pos).end(model.chain[0].end_pos).color(BLACK).stroke_weight(3.0);
-    for node in 1..model.chain_length{
-        let color = Hsl::new((node as f32)/(model.chain_length as f32)*360.0,1.0,0.5);
-        draw.line().start(model.chain[node].pos).end(model.chain[node].end_pos).color(color).stroke_weight(3.0);
+    } else {
+        draw.polyline().weight(2.0).points(model.chain.iter().map(|link| link.end_pos)).color(BLACK);
 
-
-        let current_link = Some(model.chain[node-1].clone());
-        model.chain[node].draw(&draw, &current_link, color);
     }
 
-    // draw.polyline().weight(2.0).points(model.chain.iter().map(|link| link.end_pos)).color(BLACK);
 
     
     draw.to_frame(app, &frame).unwrap();
+    
+    // app.main_window().capture_frame(captured_frame_path(&app, &frame)); // https://github.com/nannou-org/nannou/blob/master/examples/draw/draw_capture.rs
+    
     model.egui.draw_to_frame(&frame).unwrap();
+    
+}
+fn captured_frame_path(app: &App, frame: &Frame) -> std::path::PathBuf {
+    // Create a path that we want to save this frame to.
+    app.project_path()
+        .expect("failed to locate `project_path`")
+        // Capture all frames to a directory called `/<path_to_nannou>/nannou/simple_capture`.
+        .join(app.exe_name().unwrap())
+        // Name each file after the number of the frame.
+        .join(format!("{:03}", frame.nth()))
+        // The extension will be PNG. We also support tiff, bmp, gif, jpeg, webp and some others.
+        .with_extension("png")
 }
